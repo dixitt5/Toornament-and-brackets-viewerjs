fef4902c181b9b8cfb9eaf05abcc8e349d944109
dixitt5 <tilajidixit@gmail.com>
2023-06-16 10:36:19 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Toornament & brackets-viewer.js
+
+Example of use of brackets-viewer.js in React, displaying Toornament data.
+
+Note that this library doesn't support React in any way. Here, React is just a wrapper.
+
+Used:
+
+- https://github.com/Drarig29/brackets-viewer.js
+- https://github.com/Drarig29/brackets-toornament-layer
+- create-react-app
+- React
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { convertData } from "brackets-toornament-layer";
+import { InMemoryDatabase } from "brackets-memory-db";
+import { BracketsManager } from "brackets-manager";
+
+const URL =
+  "https://raw.githubusercontent.com/Drarig29/brackets-toornament-layer/59319b3d94c07d4b94ea94b580b679ede12ca046/toornament.json";
+
+// const storage = new JsonDatabase();
+// const manager = new BracketsManager(storage);
+
+async function render() {
+  const toornamentData = await fetch(URL).then((res) => res.json());
+  const data = convertData(toornamentData);
+
+  window.bracketsViewer.render({
+    stages: data.stage,
+    matches: data.match,
+    matchGames: data.match_game,
+    participants: data.participant,
+  });
+}
+
+function App() {
+  useEffect(() => {
+    render();
+  }, []);
+
+  return <div className="brackets-viewer"></div>;
+}
+
+export default App;
